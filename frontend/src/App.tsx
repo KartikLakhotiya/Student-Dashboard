7d39105483d81d8cbe16cc14cd071d8645970e50
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-06-28 17:17:27 +0530

    loader import removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FetchStudent } from './components/FetchStudent'
 import { Dashboard } from './components/Dasboard'
 import { AllStudents } from './components/AllStudents'
-import Loader from './components/loader/Loader'
 
 function App() {
 
@@ -20,7 +19,6 @@ function App() {
           <Route path='/create' element={<AddStudent />} />
           <Route path='/fetch' element={<FetchStudent />} />
           <Route path='/allstudents' element={<AllStudents />} />
-          <Route path='/loader' element={<Loader/>} />
         </Routes>
         <Toaster />
       </ThemeProvider>
