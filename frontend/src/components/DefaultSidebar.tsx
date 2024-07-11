ae7ee051e8c5b5ba1fa2609dcb7b0bd84d2bd2b0
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-07-11 17:26:39 +0530

    added default route in sidebar

--- frontend/src/components/DefaultSidebar.tsx
+++ frontend/src/components/DefaultSidebar.tsx
@@ -9,10 +9,12 @@ const DefaultSidebar = () => {
                 <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
                     <ul className="space-y-2 font-medium">
                         <li>
-                            <a className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <User2Icon />
-                                <span className="ms-3">Student Dashboard</span>
-                            </a>
+                            <Link to='/'>
+                                <a className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <User2Icon />
+                                    <span className="ms-3">Student Dashboard</span>
+                                </a>
+                            </Link>
                         </li>
                         <li>
                             <Link to='/admin'>
